66c259fed8062bd0f465631f1a38f537a435771d
Rafael Mercer <rafael_tenoriox2@hotmail.com>
2024-03-14 14:18:06 -0300

    Auth file in PHP script
    This file have a script in PHP to use for auth the logins, usernames and passwords.

--- login.php
+++ login.php
@@ -8,7 +8,7 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 
 // Verifique se a conexão foi bem-sucedida
 if ($conn->connect_error) {
-    die("Falha na conexão com o banco de dados: " . $conn->connect_error);
+    die("Failed to connect in the database: " . $conn->connect_error);
 }
 
 // Obtém os valores do formulário de login
@@ -23,14 +23,14 @@ $result = $conn->query($sql);
 // Verifica se o login foi bem-sucedido ou não
 if ($result->num_rows > 0) {
     // Login bem-sucedido, redirecionar para a página inicial ou exibir uma mensagem de sucesso
-    echo "Login bem-sucedido!";
+    echo "Welcome!";
     header("Location: http://localhost/Login-BusCompany/homepage.html");
     // Nesse caso, direciona para a página de cadastro de acidentes.
 
 } else {
     // Login falhou, redirecionar de volta ao formulário de login ou exibir uma mensagem de erro
     echo '<script>
-    alert("Nome de usuário ou senha inválidos!");
+    alert("Check your username or password!, something is wrong!");
     </script>';
 }
 
