6189db7459c1ca684e27d5b7c5188b59db09d2d0
Rafael Tenorio Gama <90986845+rafaeltenoriogama@users.noreply.github.com>
2024-03-17 02:49:21 -0300

    MySQL table to using in DB

--- users_table.sql
+++ users_table.sql
@@ -1,4 +1,4 @@
-CREATE TABLE a_sec_users (
+CREATE TABLE users_table (
   login varchar(32) NOT NULL,
   pswd varchar(255) NOT NULL,
   name varchar(64) DEFAULT NULL,
@@ -7,9 +7,7 @@ CREATE TABLE a_sec_users (
 
 INSERT INTO a_sec_users (login, pswd, name, email)
 VALUES 
-  ('ademir', 'abc123', 'Ademir Pereira', 'ademir@gmail.com'),
-  ('sabo', 'sabo123', 'Marco Sabo', 'sabo@gmail.com'),
+  ('ademir', 'abc123', 'Ademir Costa', 'ademir@gmail.com'),
+  ('marcos', 'marcos123', 'Marcos de Utah', 'sabo@gmail.com'),
   ('rafa', 'rafa123', 'Rafael Mercer', 'rafael@gmail.com');
-/* Nomes genéricos, e-mail genéricos. 
-O ideal é substituir por sua tabela de usuários já com todos os seus critérios definidos. 
-Como: E-mail, Senha, 2FA, etc. */ 
+/* Just generic names. */ 
